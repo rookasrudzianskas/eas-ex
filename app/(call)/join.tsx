8f82a41e707c6b24eccad6f8c1b312d7469794eb
rokas <rookas.rudzenskas@gmail.com>
2024-11-19 15:07:39 +0100

    fix

---
+++ app/(call)/join.tsx
@@ -0,0 +1,15 @@
+//@ts-nocheck
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const Join = () => {
+  return (
+    <View>
+      <Text>
+        byrookas 🚀
+      </Text>
+    </View>
+  );
+};
+
+export default Join;
--- scripts/reset-project.js
+++ scripts/reset-project.js
@@ -2,7 +2,7 @@
 
 /**
  * This script is used to reset the project to a blank state.
- * It moves the /app, /components, /hooks, /scripts, and /constants directories to /app-example and creates a new /app directory with an index.tsx and _layout.tsx file.
+ * It moves the /app, /components, /hooks, /scripts, and /constants directories to /app-example and creates a new /app directory with an index.tsx and join.tsx file.
  * You can remove the `reset-project` script from package.json and safely delete this file after running it.
  */
 
@@ -67,10 +67,10 @@ const moveDirectories = async () => {
     await fs.promises.writeFile(indexPath, indexContent);
     console.log("📄 app/index.tsx created.");
 
-    // Create _layout.tsx
-    const layoutPath = path.join(newAppDirPath, "_layout.tsx");
+    // Create join.tsx
+    const layoutPath = path.join(newAppDirPath, "join.tsx");
     await fs.promises.writeFile(layoutPath, layoutContent);
-    console.log("📄 app/_layout.tsx created.");
+    console.log("📄 app/join.tsx created.");
 
     console.log("\n✅ Project reset complete. Next steps:");
     console.log(
